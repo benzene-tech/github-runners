dc4a9e2040deff52aa18166733e1a8107c53b1f8
Santhosh S <santhoshsivanath@outlook.com>
2024-04-05 14:36:38 +0530

    Manage resources in Agro CD (#10)

--- infra/data.tf
+++
@@ -1,7 +0,0 @@
-data "aws_eks_cluster" "this" {
-  name = module.eks.name
-}
-
-data "aws_eks_cluster_auth" "this" {
-  name = module.eks.name
-}
--- infra/provider.tf
+++
@@ -1,9 +0,0 @@
-provider "aws" {
-  region = "ap-south-1"
-}
-
-provider "kubernetes" {
-  host                   = one(data.aws_eks_cluster.this[*].endpoint)
-  cluster_ca_certificate = base64decode(one(data.aws_eks_cluster.this[*].certificate_authority[0].data))
-  token                  = one(data.aws_eks_cluster_auth.this[*].token)
-}
